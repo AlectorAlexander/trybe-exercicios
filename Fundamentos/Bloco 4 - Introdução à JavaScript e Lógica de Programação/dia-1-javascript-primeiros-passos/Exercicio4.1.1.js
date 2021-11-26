d6b8ea8ceb8137c13d03c388fb173e839ffbe98e
Aléctor Alexander <alector.alexandre@gmail.com>
2021-11-26 19:02:03 -0300

    atualizações de arquivos

---
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/Exercicio4.1.1.js
@@ -0,0 +1,12 @@
+var status = "reprovada";
+
+switch(status){
+    case "aprovada":
+        console.log("Parabéns!");
+        break;
+    case "lista":
+        console.log("Lista de espera");
+        break;
+    case "reprovada":
+    console.log("vc é burro!")    
+}
