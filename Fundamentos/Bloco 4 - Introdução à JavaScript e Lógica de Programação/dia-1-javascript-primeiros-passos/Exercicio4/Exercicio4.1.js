b19b8158e3539bf3ebbee4de9ffbe3c96ea39115
Aléctor Alexander <alector.alexandre@gmail.com>
2021-11-28 16:46:54 -0300

    exercicio 5 feito

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/Exercicio4/Exercicio4.1.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/Exercicio4/Exercicio4.1.js
@@ -1,14 +1,11 @@
 function serasa (num){
-
-
-
     var resultado;
     if(num > 0){
         resultado = "positive";
     }else if (num < 0){
         resultado = "negative";
     }else if (num == 0){
-        resultado = "liberdade poética";
+        resultado = "a dita liberdade poética e o nada se diferem por muito pouco ou quase nada em significado. A diferença maior, mas não grande, é a de que enquanto o nada foi criado por Deus, a liberdade poética foi criada por um idiota que não sabe o que está falando";
     } else {
         resultado = "Não me disseram o que fazer nesse caso. Desculpe-me. Mas vai uma dica de como dar cambolhota: Você vai precisar de um colchonete ou pelo menos um carpete espaçoso para dar a cambalhota. Não tente fazer a técnica em pisos de azulejo, concreto ou madeira, pois o risco de acidentes é enorme."
     }
