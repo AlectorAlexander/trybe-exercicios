422dfaef6aaa59055349970f9185b5000e311a58
Aléctor Alexander <alector.alexandre@gmail.com>
2021-11-28 16:15:33 -0300

    Exercicios 4.1 pt1 and pt2 already done

---
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/Exercicio3/Exercicio4.1.js
@@ -0,0 +1,16 @@
+function serasa (num){
+
+
+
+var resultado;
+if(num > 0){
+    resultado = "positive";
+}else if (num < 0){
+    resultado = "negative";
+}else if (num == 0){
+    resultado = "liberdade poética";
+} else {
+    resultado = "Não me disseram o que fazer nesse caso. Desculpe-me. Mas vai uma dica de como dar cambolhota: Você vai precisar de um colchonete ou pelo menos um carpete espaçoso para dar a cambalhota. Não tente fazer a técnica em pisos de azulejo, concreto ou madeira, pois o risco de acidentes é enorme."
+}
+return resultado
+}
