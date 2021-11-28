731e0df988bd8ecece2a60e5854f61ea6413f728
Aléctor Alexander <alector.alexandre@gmail.com>
2021-11-28 16:28:03 -0300

    Exercicios 4.1 pt3 and pt4 already done

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/Exercicio4/Exercicio4.1.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/Exercicio4/Exercicio4.1.js
@@ -1,14 +1,14 @@
-function maioridade (a,b,c){
-    const um = a;
-    const dois = b;
-    const tres = c;
+function serasa (num){
+
+
+
     var resultado;
-    if(um > dois && um > tres){
-        resultado = um;
-    }else if (dois > um && dois > tres){
-        resultado = dois;
-    }else if (tres > um && dois < tres){
-        resultado = tres;
+    if(num > 0){
+        resultado = "positive";
+    }else if (num < 0){
+        resultado = "negative";
+    }else if (num == 0){
+        resultado = "liberdade poética";
     } else {
         resultado = "Não me disseram o que fazer nesse caso. Desculpe-me. Mas vai uma dica de como dar cambolhota: Você vai precisar de um colchonete ou pelo menos um carpete espaçoso para dar a cambalhota. Não tente fazer a técnica em pisos de azulejo, concreto ou madeira, pois o risco de acidentes é enorme."
     }
