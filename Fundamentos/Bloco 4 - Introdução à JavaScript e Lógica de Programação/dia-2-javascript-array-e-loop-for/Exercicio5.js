3b7ce8b3969054eeafed302bb6a3bd1328619334
Aléctor Alexander <alector.alexandre@gmail.com>
2021-11-29 18:52:44 -0300

    Exercicios 5 e 6 concluídos

---
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/Exercicio5.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var resultado=0;
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+
+       
+    if (numbers[index] > resultado) {
+        
+    resultado=numbers[index]
+    }
+    
+
+}console.log(resultado);
