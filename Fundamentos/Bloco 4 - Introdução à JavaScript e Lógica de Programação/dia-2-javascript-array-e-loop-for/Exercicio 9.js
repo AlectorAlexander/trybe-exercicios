211e89ba97ddb566353bce55eed7f799ca3c24b4
Aléctor Alexander <alector.alexandre@gmail.com>
2021-11-29 19:12:44 -0300

    Todos os exercicios prontos

---
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/Exercicio 9.js
@@ -0,0 +1,6 @@
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+for (let index = 1; index <= 25; index++) {
+    console.log(index / 2);
+    
+}
--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/Exercicio2.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/Exercicio2.js
@@ -1,3 +1,4 @@
+//Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 var soma = 0;
 for (var i of numbers){
