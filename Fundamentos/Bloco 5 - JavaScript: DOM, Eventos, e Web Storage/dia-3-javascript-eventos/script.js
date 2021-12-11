89450e69af8c5047710923903174abf29e64a1b8
Aléctor Alexander <alector.alexandre@gmail.com>
2021-12-11 16:04:35 -0300

    Exercicio 1 completed

--- Fundamentos/Bloco 5 - JavaScript: DOM, Eventos, e Web Storage/dia-3-javascript-eventos/script.js
+++ Fundamentos/Bloco 5 - JavaScript: DOM, Eventos, e Web Storage/dia-3-javascript-eventos/script.js
@@ -15,22 +15,36 @@ function createDaysOfTheWeek() {
   
   // Escreva seu código abaixo.
 
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  
   
   for (let index = 0; index < dezDaysList.length; index++) {
-    let dias = dezDaysList[index]
-    let lista = document.createElement("li")
+    var dias = dezDaysList[index]
+    var lista = document.createElement("li")
+    
     lista.innerText = dias;
-    lista.className = "day"
     
-    
-    
-   
-    let idays = document.getElementById("days");
-    
+       lista.className = "day"
+      
   
+    var idays = document.getElementById("days");
+     idays.appendChild(lista)
+     if (index == dezDaysList.length -1){
+       vai ()
+     }
+  }
+  function vai () {
     
-    idays.appendChild(lista)
+   for (i = 0; i <= idays.childElementCount; i += 1){
+    let lis = idays.children[i];
+    let dias = lis.innerText
+     if (dias == 24 || dias == 25 || dias == 31){
+       lis.className += " holiday "
+     }
+     if (dias == 4 || dias == 11 || dias == 18 || dias == 25){
+      lis.className += " friday"
+    }
+   }
   }
   
   
